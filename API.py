7fec2836c6f66247a760112ab6b48ce24e6cd37b
Sean Casey <sccasey04@gmail.com>
2021-11-13 12:11:44 -0800

    Set up query endpoints in settings, begin building querying functions

--- API.py
+++ API.py
@@ -1,23 +1,37 @@
 import requests
-from settings import API_KEY, API_BASE_URL, API_HOST
+from settings import API_KEY, API_BASE_URL, API_HOST, API_DIV_LEGISLATORS
+from settings import API_DIV_LEGISLATION, API_FED_LEGISLATION, API_FED_LEGISLATORS
+import json
+import pandas as pd
 
 
-def test():
+def fetch_div_legislators():
     querystring = {
-        "include_text":"true",
-        "include_actions":"true",
-        "include_votes":"true",
-        "include_summary":"true"
+        
     }
 
     headers = {
         'x-rapidapi-host': API_HOST,
         'x-rapidapi-key': API_KEY
     }
+    url = API_BASE_URL + API_DIV_LEGISLATORS + "/ca/qc"
+    # print(url)
 
-    response = requests.get(API_BASE_URL, headers=headers, params=querystring)
-    print(response.text)
+    response = requests.get(url, headers=headers, params=querystring)
+    resp_dict = json.loads(response.text)
+    json_data = resp_dict["data"]
+    pagination = resp_dict["pagination"]
+    while pagination["next_url"]:
+        url = API_BASE_URL + pagination["next_url"]
+        response = requests.get(url, headers=headers, params=querystring)
+        resp_dict = json.loads(response.text)
+        json_data.extend(resp_dict["data"])
+        pagination = resp_dict["pagination"]
+    # print(resp_dict["pagination"])
+    data = pd.json_normalize(json_data)
+    print(data)
+    print(data.columns)
 
 
 if __name__ == "__main__":
-    test()
+    fetch_div_legislators()
--- settings.py
+++ settings.py
@@ -5,4 +5,10 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY", "")
 API_HOST = "goverlytics.p.rapidapi.com"
-API_BASE_URL = "https://goverlytics.p.rapidapi.com/division-legislation/ca/bc"
+API_BASE_URL = "https://goverlytics.p.rapidapi.com"
+
+API_DIV_LEGISLATION = "/division-legislation"
+API_DIV_LEGISLATORS = "/division-legislators"
+
+API_FED_LEGISLATION = "/federal-legislation"
+API_FED_LEGISLATORS = "/federal-legislators"
